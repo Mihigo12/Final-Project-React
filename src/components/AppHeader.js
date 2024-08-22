ad0dc44180d5f784d799d4603786df0eae15a541
John <john.thom.gray@gmail.com>
2024-08-22 15:01:07 -0700

    modal

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -1,18 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from '../styles/modules/app.module.scss';
 import Button, { SelectButton } from './Button';
 import TodoModal from './TodoModal';
 
 export default function AppHeader() {
+  const [modalOpen, setModalOpen] = useState(true);
   return (
     <div className={styles.appHeader}>
-      <Button variant="primary">Add Task</Button>
+      <Button variant="primary" onClick={() => setModalOpen(true)}>
+        Add Task
+      </Button>
       <SelectButton>
         <option value="all">ALL</option>
         <option value="incomplete">Incomplete</option>
         <option value="complete">Complete</option>
       </SelectButton>
-      <TodoModal />
+      <TodoModal modalOpen={modalOpen} setModalOpen={setModalOpen} />
     </div>
   );
 }
