0f08e67d6b1e9a1b6fdf1e8b9bb334f0cf85fde1
John <john.thom.gray@gmail.com>
2024-08-27 12:40:05 -0700

    update todo;

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { format } from 'date-fns';
 import { MdDelete, MdEdit } from 'react-icons/md';
 import toast from 'react-hot-toast';
@@ -6,55 +6,65 @@ import { useDispatch } from 'react-redux';
 import { deleteTodo } from '../slices/todoSlice';
 import { getClasses } from '../utils/getClasses';
 import styles from '../styles/modules/todoItem.module.scss';
+import TodoModal from './TodoModal';
 
 function TodoItem({ todo }) {
   const dispatch = useDispatch();
+  const [updateModalOpen, setUpdateModalOpen] = useState(false);
   const handleDelete = () => {
     dispatch(deleteTodo(todo.id));
     toast.success('Todo Deleted Successfully');
   };
   const handleUpdate = () => {
-    console.log('updating');
+    setUpdateModalOpen(true);
   };
   return (
-    <div className={styles.item}>
-      <div className={styles.todoDetails}>
-        [ ]
-        <div className={styles.text}>
-          <p
-            className={getClasses([
-              styles.todoText,
-              todo.status === 'complete' && styles['todoText--completed'],
-            ])}
+    <>
+      <div className={styles.item}>
+        <div className={styles.todoDetails}>
+          [ ]
+          <div className={styles.text}>
+            <p
+              className={getClasses([
+                styles.todoText,
+                todo.status === 'complete' && styles['todoText--completed'],
+              ])}
+            >
+              {todo.title}
+            </p>
+            <p className={styles.time}>
+              {format(new Date(todo.time), 'p, MM/dd/yyyy')}
+            </p>
+          </div>
+        </div>
+        <div className={styles.todoActions}>
+          <div
+            className={styles.icon}
+            onClick={handleDelete}
+            onKeyDown={handleDelete}
+            role="button"
+            tabIndex={0}
           >
-            {todo.title}
-          </p>
-          <p className={styles.time}>
-            {format(new Date(todo.time), 'p, MM/dd/yyyy')}
-          </p>
+            <MdDelete />
+          </div>
+          <div
+            className={styles.icon}
+            onClick={handleUpdate}
+            onKeyDown={handleUpdate}
+            role="button"
+            tabIndex={0}
+          >
+            <MdEdit />
+          </div>
         </div>
       </div>
-      <div className={styles.todoActions}>
-        <div
-          className={styles.icon}
-          onClick={handleDelete}
-          onKeyDown={handleDelete}
-          role="button"
-          tabIndex={0}
-        >
-          <MdDelete />
-        </div>
-        <div
-          className={styles.icon}
-          onClick={handleUpdate}
-          onKeyDown={handleUpdate}
-          role="button"
-          tabIndex={0}
-        >
-          <MdEdit />
-        </div>
-      </div>
-    </div>
+      <TodoModal
+        type="update"
+        todo={todo}
+        modalOpen={updateModalOpen}
+        setModalOpen={setUpdateModalOpen}
+      />
+    </>
   );
 }
 
